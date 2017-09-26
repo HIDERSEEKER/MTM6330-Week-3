0aff146f09241ecab59a8a90933933d2a97bc16d
Scott Clayton <scottsdaydreams@gmail.com>
2017-09-26 15:39:15 -0400

    Add Gulp Sass complier

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp')
+
+const sass = require('gulp-sass')
+
+gulp.task('sass', function () {
+  return gulp
+  .src('src/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'))
+})
